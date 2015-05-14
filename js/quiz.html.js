fb61b9526cbc8ce342ac13f1ef6199bd5c3de286
Alex Ritzcovan <aritzcovan@gmail.com>
2015-05-14 14:43:21 -0400

    update for module pattern

--- js/quiz.html.js
+++ js/quiz.html.js
@@ -1,9 +1,37 @@
-var Quiz = function($){
+var Quiz = function ($) {
 
+    //private
     var presidents = [],
         randomPres,
 
-        init = function() {
+        getRandomPresident = function () {
+            $('#answer').val('');
+            $('#resultText').text('');
+
+            //selected a random pres
+            randomPres = presidents[Math.floor(Math.random() * presidents.length)];
+
+            //set the presidents name in the UI
+            $('#presName').text(randomPres.name);
+        },
+
+        checkAnswer = function () {
+            if ($('#answer').val() === randomPres.number) {
+                $('#resultText').text('you are correct');
+            } else {
+                $('#resultText').text('whoops! that\'s incorrect...try again');
+            }
+        },
+
+        loadPresidentList = function () {
+            presidentsJson.forEach(function (pres) {
+                presidents.push(new President(pres));
+            });
+        };
+
+    //public
+    return {
+        init: function () {
             //load the president list
             loadPresidentList();
 
@@ -13,32 +41,6 @@ var Quiz = function($){
 
             //get the first prez
             getRandomPresident();
-        },
-        getRandomPresident = function(){
-            $('#answer').val('');
-            $('#resultText').text('');
-
-            //selected a random pres
-            randomPres = presidents[Math.floor(Math.random() * presidents.length)];
-
-            //set the presidents name in the UI
-            $('#presName').text(randomPres.name);
-
-        },
-        checkAnswer = function() {
-            if ($('#answer').val() === randomPres.number) {
-                $('#resultText').text('you are correct');
-            } else {
-                $('#resultText').text('whoops! that\'s incorrect...try again');
-            }
-        },
-        loadPresidentList = function(){
-            presidentsJson.forEach(function (pres) {
-                presidents.push(new President(pres));
-            });
-        };
-
-    return {
-        init: init
-    };
+        }
+    }
 };
