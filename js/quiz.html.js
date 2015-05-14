86f672117c0db61c8167ab9c2ecdb4ac21699e23
Alex Ritzcovan <aritzcovan@gmail.com>
2015-05-14 14:09:53 -0400

    code for revealing module pattern

---
+++ js/quiz.html.js
@@ -0,0 +1,44 @@
+var Quiz = function($){
+
+    var presidents = [],
+        randomPres,
+
+        init = function() {
+            //load the president list
+            loadPresidentList();
+
+            //setup the click handlers
+            $('#btnCheckAnswer').on('click', checkAnswer);
+            $('#btnNewPresident').on('click', getRandomPresident)
+
+            //get the first prez
+            getRandomPresident();
+        },
+        getRandomPresident = function(){
+            $('#answer').val('');
+            $('#resultText').text('');
+
+            //selected a random pres
+            randomPres = presidents[Math.floor(Math.random() * presidents.length)];
+
+            //set the presidents name in the UI
+            $('#presName').text(randomPres.name);
+
+        },
+        checkAnswer = function() {
+            if ($('#answer').val() === randomPres.number) {
+                $('#resultText').text('you are correct');
+            } else {
+                $('#resultText').text('whoops! that\'s incorrect...try again');
+            }
+        },
+        loadPresidentList = function(){
+            presidentsJson.forEach(function (pres) {
+                presidents.push(new President(pres));
+            });
+        };
+
+    return {
+        init: init
+    };
+};
